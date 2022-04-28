c0477e5e7a56bd3e15f10d6c971be58d1337f811
enry1992 <hs478398@gmail.com>
2022-04-28 17:44:08 -0500

    Se agrega el proyecto

---
+++ src/closure/closure.js
@@ -0,0 +1,34 @@
+const moneyBox = (coins) => {
+    var saveCoins = 0;
+    saveCoins +=  coins;
+    console.log(`MoneyBox: $${saveCoins}`);
+}
+
+moneyBox(5);
+moneyBox(10);
+
+
+const moneyBox = () => {
+    var saveCoins = 0;
+    const countCoins = (coins) => {
+        saveCoins += coins;
+        console.log(`MoneyBox: $${saveCoins}`);
+    }
+    return countCoins;
+};
+
+let miMoneyBox = moneyBox();
+
+miMoneyBox(4);
+miMoneyBox(6);
+miMoneyBox(10);
+
+
+
+const fruits = () => {
+    if (true) {
+        var fruit1 = 'apple';
+        const fruit2 = 'banana';
+        let fruit3 = 'kiwi';
+    };
+};
